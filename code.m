a5482359da5604d853747aec7cccd4b73fbe2b4a
PhamVanNhi <vannhibka@gmail.com>
2014-05-25 12:02:18 +0700

    Update code.m

--- code.m
+++ code.m
@@ -8,7 +8,6 @@
 function bai9_word
     clc
     syms x n Sn
-    Sn=0;
     disp('              inf     ')
     disp('            %%%%%%%%%%')
     disp('              %                             ')
@@ -17,15 +16,21 @@ function bai9_word
     disp('                %     n       sqrt(n)       ')
     disp('              %                             ')
     disp('            %%%%%%%%%%%%%%%%')
-    
-    x=input('Nhap so hang can tinh chuoi, x = ')
-    disp('-----------------Tong x so hang dau tien la---------------------')
+    disp('                 n = 1                      ')
+    x=input('Nhap so hang can tinh chuoi, n = ')
+    disp('Sn = Tong n so hang dau tien la')
     Sn=double(symsum((1/n)*sin(1/sqrt(n)),n,1,x))
-    disp('-----------------Tong tu 1 den vo cung--------------------------')
-    S=double(symsum((1/n)*sin(1/sqrt(n)),n,1,inf))
-    if S~=(S+1)
-        disp('Suy ra chuoi da cho hoi tu')
+    if limit((1/n)*sin(1/sqrt(n)),n,inf)~=0
+        disp('Chuoi da cho phan ky')
     else
-        disp('Suy ra chuoi da cho  phan ky')
+        disp('S = Tong tu 1 den vo cung')
+        S=symsum((1/n)*sin(1/sqrt(n)),n,1,inf);
+        if S ~= S + 1
+            S=double(symsum((1/n)*sin(1/sqrt(n)),n,1,inf))
+            disp('Chuoi da cho hoi tu')
+        else 
+            disp('Chuoi da cho phan ky')
+        end
     end
 end
+% Update: 25/5/2014
